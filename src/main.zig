19dc9e505968bb52a0a8316367261ee3f60bdce8
vedant-pandey <vedantpandey46@gmail.com>
2025-12-10 12:36:04 +0530

    Refactor

--- src/main.zig
+++ src/main.zig
@@ -3,61 +3,127 @@ const std = @import("std");
 
 const fps = 60;
 
+const Timer = struct {
+    length: f32,
+    time: f32 = 0,
+    timeout: bool = false,
+
+    const Self = @This();
+
+    pub fn step(self: *Self, dt: f32) void {
+        self.time += dt;
+        if (self.time >= self.length) {
+            self.time -= self.length;
+            self.timeout = true;
+        }
+    }
+
+    pub fn reset(self: *Self) void {
+        self.time = 0;
+    }
+};
+
+const Animation = struct {
+    timer: Timer,
+    frameCount: i32 = 0,
+
+    pub fn currentFrame(self: *const @This()) i32 {
+        return @intFromFloat((self.timer.time / self.timer.length) * @as(f32, @floatFromInt(self.frameCount)));
+    }
+};
+
 const GameState = struct {
-    window: *sdl3.video.Window,
-    renderer: *sdl3.render.Renderer,
+    window: sdl3.video.Window = undefined,
+    renderer: sdl3.render.Renderer = undefined,
     quit: bool = false,
+    width: i32 = 0,
+    height: i32 = 0,
+    lWidth: i32 = 0,
+    lHeight: i32 = 0,
+};
+
+const Resources = struct {
+    animPlayerIdle: usize = 0,
+    playerAnims: []Animation = undefined,
+    textures: []sdl3.render.Texture = undefined,
+    texIdle: *sdl3.render.Texture = undefined,
+    len: usize = 0,
+    cap: usize,
+
+    const Self = @This();
+    pub fn loadTexture(self: *Self, state: GameState, filepath: [:0]const u8) !sdl3.render.Texture {
+        const tex = try sdl3.image.loadTexture(state.renderer, filepath);
+        self.textures[self.len] = tex;
+        self.len += 1;
+        tex.setScaleMode(.nearest) catch unreachable;
+        return tex;
+    }
+
+    pub fn load(self: *Self, state: GameState, allocator: std.mem.Allocator) !void {
+        self.textures = allocator.alloc(sdl3.render.Texture, self.cap) catch unreachable;
+        self.playerAnims = allocator.alloc(Animation, self.cap) catch unreachable;
+        self.playerAnims[self.animPlayerIdle] = Animation{
+            .timer = Timer{ .length = 8, .time = 1.8 },
+        };
+        self.texIdle.* = try self.loadTexture(state, "data/idle.png");
+    }
+
+    pub fn unload(self: *Self, allocator: std.mem.Allocator) void {
+        for (self.textures) |*tex| {
+            tex.deinit();
+        }
+        allocator.free(self.textures);
+        allocator.free(self.playerAnims);
+    }
 };
 
 pub fn main() !void {
-    var screenWidth: i32 = 1521;
-    var screenHeight: i32 = 1375;
+    var state: GameState = .{
+        .width = 1521,
+        .height = 1375,
+        .lHeight = 320,
+        .lWidth = 640,
+    };
 
     defer sdl3.shutdown();
 
-    // Initial config Start
-    const init_flags = sdl3.InitFlags{ .video = true };
-    try sdl3.init(init_flags);
-    defer sdl3.quit(init_flags);
+    var arena = std.heap.ArenaAllocator.init(std.heap.c_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
 
-    var window = sdl3.video.Window.init("Hello SDL3", @intCast(screenWidth), @intCast(screenHeight), .{ .always_on_top = true }) catch {
-        try sdl3.message_box.showSimple(.{ .error_dialog = true }, "Error", "Error creating window", null);
-        return error.SDLWindowInitFailed;
-    };
-    defer window.deinit();
-
-    var renderer = sdl3.render.Renderer.init(window, null) catch {
-        try sdl3.message_box.showSimple(.{ .error_dialog = true }, "Error", "Error creating window", null);
-        return error.SDLWindowInitFailed;
-    };
-    defer renderer.deinit();
-    var state: GameState = .{
-        .window = &window,
-        .renderer = &renderer,
-    };
-
-    try state.window.raise();
-    const displays = try sdl3.video.getDisplays();
-    if (displays.len > 1) {
-        const display2 = try displays[1].getBounds();
-        try state.window.setPosition(.{ .absolute = display2.x }, .{ .absolute = display2.y });
+    if (!initializeGame(&state)) {
+        return error.GameInitializationFailed;
     }
-    // Initial config End
+
+    defer cleanup(&state);
 
     // Configuration
-    const lWidth = 640;
-    const lHeight = 320;
     state.window.setResizable(true) catch unreachable;
-    try state.renderer.setLogicalPresentation(lWidth, lHeight, .letter_box);
+    state.renderer.setLogicalPresentation(@intCast(state.lWidth), @intCast(state.lHeight), .letter_box) catch |err| {
+        std.debug.print("Error {}\n", .{err});
+        return err;
+    };
+
+    // Game data
+    const keyStates = sdl3.keyboard.getState();
+    var playerX: f32 = 15.0;
+    const floor: f32 = @floatFromInt(state.lHeight);
 
     // Load assets
-    const idleTex = try sdl3.image.loadTexture(state.renderer.*, "data/idle.png");
-    defer idleTex.deinit();
+    var res: Resources = .{ .cap = 5 };
+    res.load(state, allocator) catch {};
+    defer res.unload(allocator);
 
-    try idleTex.setScaleMode(.nearest);
+    var prevTime = sdl3.c.SDL_GetTicks();
+
+    var flipHorizontal = false;
 
     // Game loop
     while (!state.quit) {
+        const nowTime = sdl3.c.SDL_GetTicks();
+        const dt: f32 = @as(f32, @floatFromInt(nowTime - prevTime)) / 1000.0;
+        prevTime = nowTime;
+
         while (sdl3.events.poll()) |event| {
             switch (event) {
                 .quit => state.quit = true,
@@ -68,8 +134,8 @@ pub fn main() !void {
                     }
                 },
                 .window_resized => {
-                    screenWidth = event.window_resized.width;
-                    screenHeight = event.window_resized.height;
+                    state.width = event.window_resized.width;
+                    state.height = event.window_resized.height;
                 },
                 else => {},
             }
@@ -79,18 +145,78 @@ pub fn main() !void {
         state.renderer.clear() catch unreachable;
         defer state.renderer.present() catch unreachable;
 
+        const spriteSize = 32;
+
         const src = sdl3.rect.FRect{
             .x = 0,
             .y = 0,
-            .w = 32,
-            .h = 32,
+            .w = spriteSize,
+            .h = spriteSize,
         };
         const dst = sdl3.rect.FRect{
-            .x = 0,
-            .y = 0,
-            .w = 32,
-            .h = 32,
+            .x = playerX,
+            .y = floor - spriteSize,
+            .w = spriteSize,
+            .h = spriteSize,
         };
-        try state.renderer.renderTexture(idleTex, src, dst);
+
+        var movement: f32 = 0;
+        if (keyStates[@intFromEnum(sdl3.Scancode.a)]) {
+            movement += -75.0;
+            flipHorizontal = true;
+        }
+        if (keyStates[@intFromEnum(sdl3.Scancode.d)]) {
+            movement += 75.0;
+            flipHorizontal = false;
+        }
+        playerX += movement * dt;
+
+        try state.renderer.renderTextureRotated(res.texIdle.*, src, dst, 0, null, .{ .horizontal = flipHorizontal });
     }
+}
+
+const init_flags = sdl3.InitFlags{ .video = true };
+pub fn cleanup(state: *GameState) void {
+    defer sdl3.quit(init_flags);
+    state.window.deinit();
+    state.renderer.deinit();
+}
+
+pub fn initializeGame(state: *GameState) bool {
+    sdl3.init(init_flags) catch {
+        showErrorDialog("Error initializing SDL");
+        return false;
+    };
+
+    var window = sdl3.video.Window.init("Hello SDL3", @intCast(state.width), @intCast(state.height), .{ .always_on_top = true }) catch {
+        showErrorDialog("Error creating window");
+        return false;
+    };
+
+    var renderer = sdl3.render.Renderer.init(window, null) catch {
+        showErrorDialog("Error creating renderer");
+        window.deinit();
+        return false;
+    };
+
+    state.window = window;
+    state.renderer = renderer;
+
+    state.window.raise() catch {
+        showErrorDialog("Error raising window");
+        window.deinit();
+        renderer.deinit();
+        return false;
+    };
+
+    const displays = sdl3.video.getDisplays() catch unreachable;
+    if (displays.len > 1) {
+        const display2 = displays[1].getBounds() catch unreachable;
+        state.window.setPosition(.{ .absolute = display2.x }, .{ .absolute = display2.y }) catch unreachable;
+    }
+    return true;
+}
+
+pub fn showErrorDialog(message: [:0]const u8) void {
+    sdl3.message_box.showSimple(.{ .error_dialog = true }, "Error", message, null) catch unreachable;
 }
